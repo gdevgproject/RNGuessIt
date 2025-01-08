7e2fced48032ce94c50e547c4cb16720520ebdb9
Vũ Đức Minh <gdev.gproject@gmail.com>
2025-01-09 06:39:27 +0700

    wip: UI type my number

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.appContainer}>
+        <View>
+          <Text>Guess My Number</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <Text>Enter a Number</Text>
+          <TextInput placeholder="____" />
+          <View style={styles.buttonContainer}>
+            <Pressable style={styles.button}>
+              <Text>Reset</Text>
+            </Pressable>
+            <Pressable style={styles.button}>
+              <Text>Confirm</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputContainer: {
+    backgroundColor: "",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    backgroundColor: "#71063c",
+    padding: 10,
+    borderRadius: 20,
+    paddingHorizontal: 40,
   },
 });
